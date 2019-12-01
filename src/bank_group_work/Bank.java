d65e232017003ec3e31e31556380971a7d9ff3cc
Piotr <pblajer@gmail.com>
2019-12-01 10:17:15 +0100

    Bank class initial template

---
+++ src/bank_group_work/Bank.java
@@ -0,0 +1,15 @@
+package bank_group_work;
+import java.util.List;
+
+
+    public class Bank {
+    private String bankName;
+
+
+    public Bank(String bankName) {
+        this.bankName = bankName;
+    }
+    public String getName() {
+        return bankName;
+    }
+    }

354c18f593a45db284acae218f37ea391c97c8d5
Piotr <pblajer@gmail.com>
2019-12-01 12:21:04 +0100

    Client class pesel field + getter

---
+++ src/bank_group_work/Account.java
@@ -0,0 +1,27 @@
+package bank_group_work;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private String accountNumber;
+    private AccountType accountType;
+    private BigDecimal balance;    public Account(String accountNumber, AccountType accountType, BigDecimal balance) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+    }    public Account(String accountNumber, AccountType accountType) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance=BigDecimal.ZERO;
+    }    public String getAccountNumber() {
+
+        return accountNumber;
+    }
+    public AccountType getAccountType() {
+
+        return accountType;
+    }
+    public BigDecimal getBalance() {
+        return balance;
+    }
+}
---
+++ src/bank_group_work/Client.java
@@ -0,0 +1,10 @@
+package bank_group_work;
+
+public class Client {
+    private String name, surname;
+    private String pesel;
+
+    public String getPesel() {
+        return pesel;
+    }
+}
